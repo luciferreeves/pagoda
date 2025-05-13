e59b46fd569d5868eb759f7740dbaad8251d6034
Bobby <bobbyskhs@gmail.com>
2025-05-13 09:04:30 +0530

    moved district repeated code to component; added district to arcadia

--- src/data/districts/arcadia.ts
+++ src/data/districts/arcadia.ts
@@ -1,3 +1,15 @@
 import type { Site } from "./types";
 
-export const arcadia: Site[] = [];
+export const arcadia: Site[] = [
+  {
+    name: "The Great Pretender",
+    url: "https://thegreatpretender02.neocities.org/",
+    description:
+      "Howdy! You can call me Eve or Pretender—that’s what I go by online (and in my head, lol). I’m a 22-year-old gal who uses she/her pronouns, and I live in Texas, aka cowboy land. Yeehaw! 🤠🐄 <br><br> I’m extremely online, which is one reason I created this website. I’ve always been a creative person, even if my skills aren’t perfect—I have to express myself somehow! My journey started with Carrd, but I wanted more freedom, so here we are.<br><br> A little about me: I love gaming, drawing, writing, roleplaying, and yapping about my hyperfixations.",
+    owner: "thegreatpretender02",
+    tags: ["art", "videogames", "startrek", "oldies", "personal"],
+    added: new Date("2025-05-13 08:50"),
+    screenshotUrl:
+      "https://neocities.org/site_screenshots/14/07/thegreatpretender02/index.html.540x405.webp",
+  },
+];
