9246437b4b9f56af9c8671a9b26d37323829bb32
Bobby <30593201+luciferreeves@users.noreply.github.com>
2025-05-17 02:23:48 +0530

    Added Shifoo to Tokyo

--- src/data/districts/tokyo.ts
+++ src/data/districts/tokyo.ts
@@ -1,3 +1,15 @@
 import type { Site } from "./types";
 
-export const tokyo: Site[] = [];
+export const tokyo: Site[] = [
+  {
+    name: "Shifoo (That Computer Scientist)",
+    url: "https://crvs.neocities.org/",
+    description:
+      "Welcome to the home of Shifoo (previously That Computer Scientist). My name is Bobby, and this is my personal website. I aim to build a retro looking personal website, where I share my thoughts, ideas, and experiences through articles, and will showcase some cool nostalgic features and tools.",
+    owner: "crvs",
+    tags: ["anime", "personal", "community", "programming", "blog"],
+    added: new Date("2025-05-16 02:22"),
+    screenshotUrl:
+      "https://neocities.org/site_screenshots/40/23/crvs/index.html.540x405.webp",
+  },
+];
