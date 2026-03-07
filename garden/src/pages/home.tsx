8db94ce0ccb4c5e790a2e0d346b2cbbf411b22b8
Bobby <30593201+luciferreeves@users.noreply.github.com>
2026-03-07 12:30:05 +0530

    feat: add construction notice to Home page

--- garden/src/pages/home.tsx
+++ garden/src/pages/home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <section>
-      <p></p>
+      <p>Pagoda is currently under construction. You can browse around and test out the features, but multiple data wipes will happen as changes are made. There will be no notice before wipes, so be sure to not put anything important in here. Best to just play around, have fun and experiment!</p>
     </section>
   );
 }
